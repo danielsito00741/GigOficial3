278f2332b11408d9af26a9bcb3c5f4118f8c5b41
Dani Llanos <daniel00741@gmail.com>
2017-10-24 23:52:54 -0500

    Create Gig and Genre Tables

---
+++ GigOficial3/GigOficial3/Models/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace GigOficial3.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ GigOficial3/GigOficial3/Models/Gig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GigOficial3.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+        public ApplicationUser Artist { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venue { get; set; } 
+        public Genre Genre { get; set; }
+    }
+}
