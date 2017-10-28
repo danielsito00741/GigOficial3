55567e12001b94e13b3ea6f60293df14af010b16
Dani Llanos <daniel00741@gmail.com>
2017-10-27 20:51:24 -0500

    Saving Data

--- GigOficial3/GigOficial3/Controllers/GigsController.cs
+++ GigOficial3/GigOficial3/Controllers/GigsController.cs
@@ -1,5 +1,6 @@
 ﻿using GigOficial3.Models;
 using GigOficial3.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +19,7 @@ namespace GigOficial3.Controllers
             _context = new ApplicationDbContext();
         }
 
-        // GET: Gigs
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -26,6 +27,27 @@ namespace GigOficial3.Controllers
                 Genres = _context.Genres.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var artistId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == artistId);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            var gig = new Gig
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
